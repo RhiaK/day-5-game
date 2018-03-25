3bd74142cfcd8a33e0d0ba54ff426c093d12182b
RhiaKe <rhiakaulitzke@gmail.com>
2018-03-25 11:56:03 -0600

    added some js stuff, changed some html

---
+++ app.js
@@ -0,0 +1,30 @@
+//I need to get car1 to move right when the right arrow is pushed
+//car2 needs to go right by one table space when the "D" button is pushed, attach to table?
+//they need to stop when they reach the sign
+//check for winner
+//display winner
+
+let first = [{image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0O981pFeBSJiiANkGNeIJCdmCMFYW2F8fI80bk4ckCbgNlDldPw"}, 
+"acar", "bcar", "ccar", "dcar", "ecar", "fcar", "gcar", "hcar", "icar", "jcar", "kcar"];
+
+let second = [{image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7h1576-FhgbaVBkqI44gmEP6s4HdsjfSbcaaq2SkVAu3D-0PnEA"},"aca2", "bca2", "cca2", "dca2", "eca2", "fca2", "gca2", "hca2", "ica2", "jca2", "kca2"];
+
+console.log(first[0]);
+
+
+//first.addEventListener("keydown",() => {
+
+//	alert("yea");
+
+//});
+
+
+
+
+
+
+
+
+
+
+//if button press, remove last space in array, right justify
